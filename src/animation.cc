eb6ede742f4a480b61acc8411ea5221b39d62cf3
Dan Green <danngreen1@gmail.com>
2022-10-12 14:37:19 -0700

    Added LED animations

--- src/animation.cc
+++ src/animation.cc
@@ -16,92 +16,96 @@ void animate(Animations animation_type) {
 	switch (animation_type) {
 
 		case ANI_RESET:
-			// set_rgb_led(RgbLeds::Ping, Palette::Black);
-			// set_rgb_led(RgbLeds::Cycle, Palette::Black);
-
 			last_tm = cur_tm;
 			ctr = 0;
+			LED_LOOP1_OFF;
+			LED_LOOP2_OFF;
+			LED_INF1_OFF;
+			LED_INF2_OFF;
+			LED_REV1_OFF;
+			LED_REV2_OFF;
+			LED_PINGBUT_OFF;
 			break;
 
 		case ANI_SUCCESS:
-			// if (ctr == 0) {
-			// 	set_rgb_led(RgbLeds::Ping, Palette::Red);
-			// 	set_rgb_led(RgbLeds::Cycle, Palette::Red);
-			// } else if (ctr == 1) {
-			// 	set_rgb_led(RgbLeds::Ping, Palette::Yellow);
-			// 	set_rgb_led(RgbLeds::Cycle, Palette::Yellow);
-			// } else if (ctr == 2) {
-			// 	set_rgb_led(RgbLeds::Ping, Palette::White);
-			// 	set_rgb_led(RgbLeds::Cycle, Palette::White);
-			// } else if (ctr == 3) {
-			// 	set_rgb_led(RgbLeds::Ping, Palette::Cyan);
-			// 	set_rgb_led(RgbLeds::Cycle, Palette::Cyan);
-			// } else if (ctr == 4) {
-			// 	set_rgb_led(RgbLeds::Ping, Palette::Green);
-			// 	set_rgb_led(RgbLeds::Cycle, Palette::Green);
-			// } else if (ctr == 5) {
-			// 	set_rgb_led(RgbLeds::Ping, Palette::Blue);
-			// 	set_rgb_led(RgbLeds::Cycle, Palette::Blue);
-			// } else
-			// 	ctr = 0;
+			step_time = 200 * TICKS_PER_MS;
+			if (ctr == 0) {
+				LED_PINGBUT_ON;
+
+				LED_REV2_OFF;
+				LED_REV1_ON;
+			} else if (ctr == 1) {
+				LED_REV1_OFF;
+				LED_INF1_ON;
+			} else if (ctr == 2) {
+				LED_PINGBUT_OFF;
+
+				LED_INF1_OFF;
+				LED_INF2_ON;
+			} else if (ctr == 3) {
+				LED_INF2_OFF;
+				LED_REV2_ON;
+			} else
+				ctr = 0;
 			break;
 
 		case ANI_WAITING:
-			//Flash button green/off when waiting
-			// if (ctr == 0)
-			// 	set_rgb_led(RgbLeds::Ping, Palette::Black);
-			// else if (ctr == 1)
-			// 	set_rgb_led(RgbLeds::Ping, Palette::Green);
-			// else
-			// 	ctr = 0;
+			LED_LOOP1_OFF;
+			LED_LOOP2_OFF;
+			if (ctr == 0) {
+				LED_PINGBUT_ON;
+			} else if (ctr == 1) {
+				LED_PINGBUT_OFF;
+			} else
+				ctr = 0;
 			break;
 
 		case ANI_RECEIVING:
-			step_time = 200 * TICKS_PER_MS;
-			// if (ctr == 0) {
-			// 	set_rgb_led(RgbLeds::Ping, Palette::Blue);
-			// 	set_rgb_led(RgbLeds::Cycle, Palette::White);
-			// } else if (ctr == 1) {
-			// 	set_rgb_led(RgbLeds::Ping, Palette::White);
-			// 	set_rgb_led(RgbLeds::Cycle, Palette::Blue);
-			// } else
-			// 	ctr = 0;
+			step_time = 100 * TICKS_PER_MS;
+			if (ctr == 0) {
+				LED_LOOP1_ON;
+			} else if (ctr == 1) {
+				LED_LOOP1_OFF;
+			} else
+				ctr = 0;
 			break;
 
 		case ANI_SYNC:
-			step_time = 100 * TICKS_PER_MS;
-			// if (ctr == 0) {
-			// 	set_rgb_led(RgbLeds::Ping, Palette::Black);
-			// 	set_rgb_led(RgbLeds::Cycle, Palette::Black);
-			// } else if (ctr == 1) {
-			// 	set_rgb_led(RgbLeds::Ping, Palette::Magenta);
-			// 	set_rgb_led(RgbLeds::Cycle, Palette::Black);
-			// } else
-			// 	ctr = 0;
+			step_time = 50 * TICKS_PER_MS;
+			if (ctr == 0) {
+				LED_LOOP1_ON;
+				LED_LOOP2_ON;
+			} else if (ctr == 1) {
+				LED_LOOP1_OFF;
+				LED_LOOP2_OFF;
+			} else
+				ctr = 0;
 			break;
 
 		case ANI_WRITING:
-			step_time = 100 * TICKS_PER_MS;
-			// if (ctr == 0) {
-			// 	set_rgb_led(RgbLeds::Ping, Palette::Black);
-			// 	set_rgb_led(RgbLeds::Cycle, Palette::Yellow);
-			// } else if (ctr == 1) {
-			// 	set_rgb_led(RgbLeds::Ping, Palette::Yellow);
-			// 	set_rgb_led(RgbLeds::Cycle, Palette::Black);
-			// } else
-			// 	ctr = 0;
+			step_time = 50 * TICKS_PER_MS;
+			if (ctr == 0) {
+				LED_LOOP2_ON;
+			} else if (ctr == 1) {
+				LED_LOOP2_OFF;
+			} else
+				ctr = 0;
 			break;
 
 		case ANI_FAIL_ERR:
-			step_time = 100 * TICKS_PER_MS;
-			// if (ctr == 0) {
-			// 	set_rgb_led(RgbLeds::Ping, Palette::Black);
-			// 	set_rgb_led(RgbLeds::Cycle, Palette::Red);
-			// } else if (ctr == 1) {
-			// 	set_rgb_led(RgbLeds::Ping, Palette::Red);
-			// 	set_rgb_led(RgbLeds::Cycle, Palette::Black);
-			// } else
-			// 	ctr = 0;
+			LED_LOOP1_ON;
+			LED_LOOP2_ON;
+			LED_INF1_ON;
+			LED_INF2_ON;
+			LED_REV2_ON;
+			LED_PINGBUT_ON;
+			if (ctr == 0) {
+				LED_REV1_ON;
+			} else if (ctr == 1) {
+				LED_REV1_OFF;
+			} else
+				ctr = 0;
+			break;
 			break;
 
 		default:
